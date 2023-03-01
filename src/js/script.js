bc091f1523cf7a4a3ed92646152758c8cb71260a
misha <misha0202-15@hotmail.com>
2023-03-01 15:12:27 +0100

    learn callback

--- src/js/script.js
+++ src/js/script.js
@@ -1,81 +1,27 @@
 "use strict";
 
-let number0Films;
- 
-function start (){
-number0Films =  +prompt ('combien de films avez vous regarder?','');
- while (number0Films == ''|| number0Films == null || isNaN(number0Films)){
-    number0Films =  +prompt ('combien de films avez vous regarder?','');
-  }
-}
+function first (){
+setTimeout(function(){
+console.log(1);
+}, 500);
 
-start();
-
-
-
-    const personnalMovieDB = {
-        count: number0Films,
-        movies:{},
-        actors:{},
-        genre: [],
-        privat:false
-
-};
-
-
-
-
-function rememberMyfilms(){
-
-for ( let i = 0; i <2; i++){
-    const a = prompt ( 'le dernier film visioner ?', ''),
-    b = prompt ('avez vous apprécier?','');
-
-    if (a != null && b != null && a  != "" &&  b != "" && a.length <50 ){
-        personnalMovieDB.movies[a] = b;
-        console.log('done');
-     } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
-//rememberMyfilms();
-
-
-function detectPersonnallevel(){
-
-    if(personnalMovieDB.count<10){
-        console.log("peu de film visioner");
-      }else if (personnalMovieDB.count >=10 && personnalMovieDB.count >30)
-       console.log("vous etes un utilisateur classique ");
-   else if (personnalMovieDB.count >=30 ){
-        console.log("vous etes un habituer ");
-  }else{ console.log("une arreur est survenue");
-  
-  }
-}
-
-//detectPersonnallevel();
-
-function showMyDB(hidden){
-if (!hidden) {
-    console.log(personnalMovieDB);
 
 }
-}
-  showMyDB(personnalMovieDB.privat);
+function second(){
+console.log(2);
 
-console.log(personnalMovieDB);
-
-//cycle se  commence a 1 et se repete 3 fois 
-
-function writeYourGenres(){
-for(let i = 1; i <=3; i++ ) {
-
-personnalMovieDB.genre[i - 1] = prompt(`votre genre préferer ${i}`);
-}
 
 }
+first();
+second();
+function learnJS ( lang , callback){
+console.log( `japprend :${lang}`);
+callback();
+}
+function done(){
+    console.log('étudier ce cours');
+}
 
-writeYourGenres ();
+// on fait appel a la fonction done pour qu"elle qoit utliser a un moment donner
+learnJS('Javsacript', done);
+   
