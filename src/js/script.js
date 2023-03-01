9007e3c831a20e99e04bf044443a2de990328d4e
misha <misha0202-15@hotmail.com>
2023-03-01 14:33:35 +0100

    create functions script

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,81 @@
 "use strict";
 
-const nuber0Films = +prompt ('conbien de films avez vousb regarder?','');
-const personnalMovieDB = {
-         count: nuber0Films,
-         movies:{},
-         actors:{},
-         genre: [],
-         privat:false
+let number0Films;
+ 
+function start (){
+number0Films =  +prompt ('combien de films avez vous regarder?','');
+ while (number0Films == ''|| number0Films == null || isNaN(number0Films)){
+    number0Films =  +prompt ('combien de films avez vous regarder?','');
+  }
+}
+
+start();
+
+
+
+    const personnalMovieDB = {
+        count: number0Films,
+        movies:{},
+        actors:{},
+        genre: [],
+        privat:false
 
 };
-const a = prompt ( 'le dernier film visioner ?', ''),
-b = prompt ('avez vous apprécier?',''),
-c = prompt ('le dernier film visioner?', ''), 
-d = prompt ( 'На сколько оцените его?','');
 
-personnalMovieDB.movies[a]=b;
-personnalMovieDB.movies[c]=d;
+
+
+
+function rememberMyfilms(){
+
+for ( let i = 0; i <2; i++){
+    const a = prompt ( 'le dernier film visioner ?', ''),
+    b = prompt ('avez vous apprécier?','');
+
+    if (a != null && b != null && a  != "" &&  b != "" && a.length <50 ){
+        personnalMovieDB.movies[a] = b;
+        console.log('done');
+     } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+//rememberMyfilms();
+
+
+function detectPersonnallevel(){
+
+    if(personnalMovieDB.count<10){
+        console.log("peu de film visioner");
+      }else if (personnalMovieDB.count >=10 && personnalMovieDB.count >30)
+       console.log("vous etes un utilisateur classique ");
+   else if (personnalMovieDB.count >=30 ){
+        console.log("vous etes un habituer ");
+  }else{ console.log("une arreur est survenue");
+  
+  }
+}
+
+//detectPersonnallevel();
+
+function showMyDB(hidden){
+if (!hidden) {
+    console.log(personnalMovieDB);
+
+}
+}
+  showMyDB(personnalMovieDB.privat);
 
 console.log(personnalMovieDB);
 
-   
+//cycle se  commence a 1 et se repete 3 fois 
+
+function writeYourGenres(){
+for(let i = 1; i <=3; i++ ) {
+
+personnalMovieDB.genre[i - 1] = prompt(`votre genre préferer ${i}`);
+}
+
+}
+
+writeYourGenres ();
